04bcd955836c30b2a25fd9ccc70811dd5ed82c71
AleksandrV2010 <avyatkin2010@gmail.com>
2026-01-13 19:35:26 -0500

    I am Cool

---
+++ class Car.py
@@ -0,0 +1,10 @@
+class Car
+
+    def __init__(self, color, number, company):
+        self.color = color
+        self.number = number
+        self.company = company
+
+    def display_info(self):
+        return f"Car color is {self.color}, the number is {self.number}, and the company is {self.company}."::
+
